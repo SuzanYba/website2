7a9e433331888b759fbd760939b32876ea12edbd
SuzanYba <108312100+SuzanYba@users.noreply.github.com>
2022-09-26 22:24:53 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+var typed = new Typed(".autotype",{
+    strings:["A Web Developer","A Student","An Artist","A Song Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}) 
